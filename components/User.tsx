e16bce9a9ea85bb85629cd9b8b1b44d1af065bfe
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-05 22:50:10 +0530

    Password and Email Input fields

--- components/User.tsx
+++ components/User.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Button, Text, View, StyleSheet, TextInput } from 'react-native';
+import { Button, Text, View, StyleSheet, TextInput, Alert } from 'react-native';
 
 {/*
     
@@ -7,37 +7,107 @@ import { Button, Text, View, StyleSheet, TextInput } from 'react-native';
 
 const User = () : JSX.Element => {
 
-    const [fname,setFname] = useState("");
-    const [lname,setLname] = useState("");
+    const [name,setName] = useState("");
+    const [email,setEmail] = useState("");
+    const [password,setPassword] = useState("");
+    const [display,setDisplay] = useState(false);
+    const [isShow,setIsShow] = useState(true);
     return (
-        <View>
-            <Text style={{fontSize : 30,color : '#000',margin : 5}}>Input Field</Text>
-            <Text style={{fontSize : 20,color : '#000',margin : 5}}>Enter Your name : </Text>
+        <View style={{
+            padding : 10
+        }}>
+            <Text style={{fontSize : 30,color : '#000'}}>Input Field</Text>
+            <Text style={{fontSize : 20,color : '#000',marginTop : 20}}>Enter Your Name : </Text>
             <TextInput 
-                style={{fontSize : 20,color : '#000',margin : 5,borderColor : 'black',borderWidth : 1,borderRadius : 5}}
+                style={styles.inputStyle}
                 onChangeText={(value)=>{
-                    setFname(value);
+                    setName(value);
                 }}
-                value={fname}
-                id='firstName'
+                value={name}
             ></TextInput>
+            <Text style={{fontSize : 20,color : '#000',marginTop : 20}}>Enter Your Email : </Text>
             <TextInput 
-                style={{fontSize : 20,color : '#000',margin : 5,borderColor : 'black',borderWidth : 1,borderRadius : 5}}
+                style={styles.inputStyle}
                 onChangeText={(value)=>{
-                    setLname(value);
+                    setEmail(value);
                 }}
-                id='lastName'
-                value={lname}
+                value={email}
+                autoCapitalize='none'
+                keyboardType='email-address'
+               
             ></TextInput>
+            
+            <Text style={{fontSize : 20,color : '#000',marginTop : 20}}>Enter Your Password : </Text>
+            <View style={{
+                marginBottom : 3
+            }}>
+
+            <TextInput 
+                style={styles.inputStyle}
+                onChangeText={(value)=>{
+                    setPassword(value);
+                }}
+                value={password}
+                secureTextEntry={isShow}
+                ></TextInput>
+                <Button 
+                    title='Show Password'
+                    onPress={()=>{
+                        setIsShow(!isShow);
+                    }}
+                ></Button>
+            </View>
+            
+
+            <Button title='Submit'
+            color={'red'}
+            onPress={()=>{
+                setDisplay(!display);
+            }}
+            touchSoundDisabled={true}
+            ></Button>
+            <Text ></Text>
             <Button title='Clear' onPress={()=>{
-                setFname("");
-                setLname("");
-            }}></Button>
-            <Text style={{fontSize : 20,color : '#000',margin : 5}}>{fname}</Text>
-            <Text style={{fontSize : 20,color : '#000',margin : 5}}>{lname}</Text>
+                setName("");
+                setEmail("");
+                setPassword("");
+            }}
+            touchSoundDisabled={true}
+            ></Button>
+            {
+                    display ? 
+            <View style={{
+                backgroundColor : 'green',
+                borderColor :'black',
+                borderWidth : 2,
+                borderBottomWidth : 10,
+                borderRadius : 5,
+                marginTop : 20
+            }}>
+                
+                    <View>
+                        <Text style={{fontSize : 20,color : '#fff',margin : 5}}>Name     : {name}</Text>
+                        <Text style={{fontSize : 20,color : '#fff',margin : 5}}>Email    : {email}</Text>
+                        <Text style={{fontSize : 20,color : '#fff',margin : 5}}>Password : {password}</Text>
+                    </View> 
+               
+            </View> : null
+             }
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    inputStyle : {
+        fontSize : 20,
+        color : '#000',
+        borderColor : 'black',
+        borderWidth : 1,
+        borderRadius : 5,
+        marginBottom : 12,
+        paddingLeft : 15,
+        
+    }
+})
 
 export default User;
