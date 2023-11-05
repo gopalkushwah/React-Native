c0a20b4b88392b944f285a4c778b581bb431df4e
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-05 20:29:40 +0530

    TextInput - taking input from user

--- components/User.tsx
+++ components/User.tsx
@@ -1,37 +1,43 @@
 import React, { useState } from 'react'
-import { Button, Text, View, StyleSheet } from 'react-native';
-import styles1 from './../style/style';
+import { Button, Text, View, StyleSheet, TextInput } from 'react-native';
 
 {/*
     
 */}
 
 const User = () : JSX.Element => {
+
+    const [fname,setFname] = useState("");
+    const [lname,setLname] = useState("");
     return (
         <View>
-            {/* Inline Style */}
-            <Text style={{
-                fontSize : 30,
-                color : 'red'
-            }}>hello</Text>
-
-            {/* Intenal Style */}
-            <Text style={styles.text}>Gopal</Text>
-
-            {/* External Style */}
-            <Text style={styles1.text}>Kushwah</Text>
-
-
+            <Text style={{fontSize : 30,color : '#000',margin : 5}}>Input Field</Text>
+            <Text style={{fontSize : 20,color : '#000',margin : 5}}>Enter Your name : </Text>
+            <TextInput 
+                style={{fontSize : 20,color : '#000',margin : 5,borderColor : 'black',borderWidth : 1,borderRadius : 5}}
+                onChangeText={(value)=>{
+                    setFname(value);
+                }}
+                value={fname}
+                id='firstName'
+            ></TextInput>
+            <TextInput 
+                style={{fontSize : 20,color : '#000',margin : 5,borderColor : 'black',borderWidth : 1,borderRadius : 5}}
+                onChangeText={(value)=>{
+                    setLname(value);
+                }}
+                id='lastName'
+                value={lname}
+            ></TextInput>
+            <Button title='Clear' onPress={()=>{
+                setFname("");
+                setLname("");
+            }}></Button>
+            <Text style={{fontSize : 20,color : '#000',margin : 5}}>{fname}</Text>
+            <Text style={{fontSize : 20,color : '#000',margin : 5}}>{lname}</Text>
         </View>
     )
 }
 
-const styles = StyleSheet.create({
-    text :{
-        color : 'green',
-        fontSize : 35,
-        marginTop : 5
-    }
-})
 
 export default User;
