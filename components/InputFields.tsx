67a2315b45c55096a8024b3ef38ef0406922ccc5
Gopal Kushwah <118118741+gopalkushwah@users.noreply.github.com>
2023-11-06 00:24:56 +0530

    Update InputFields.tsx

--- components/InputFields.tsx
+++ components/InputFields.tsx
@@ -1,1 +1,26 @@
+import React from 'react'
+import {TextInput,View} from 'react-native'
 
+const InputField = () => {
+  return(
+      <View>
+          {/*
+            to create a multi line input field
+          */}
+          <TextInput 
+            multiline={true}
+            style ={styles.inputStyle}
+
+            {/* 
+              height will become of 5 lines only , it will take more then 5 line but it height will be of 5
+            */}
+            numberOfLines={5}
+            
+            ></TextInput>
+
+        
+      </View>
+
+    
+  )
+}
