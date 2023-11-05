9e5af2f0ffe6d62e4bb85eae940bce0d9f93a2d8
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-05 23:50:41 +0530

    List using FlatList

--- components/User.tsx
+++ components/User.tsx
@@ -1,98 +1,65 @@
-import React, { useState } from 'react'
-import { Button, Text, View, StyleSheet, TextInput, Alert } from 'react-native';
+import React from 'react'
+import { Text, View, StyleSheet, FlatList } from 'react-native';
 
 {/*
     
 */}
+const data = [
+    {
+        id : 1,
+        name : 'Gopal'
+    },
+    {
+        id : 2,
+        name : 'Anish'
+    },
+    {
+        id : 3,
+        name : 'Satyam'
+    },
+    {
+        id : 4,
+        name : 'Ram'
+    },
+    {
+        id : 5,
+        name : 'Sumit'
+    },
+    {
+        id : 6,
+        name : 'Ritik'
+    },
+    {
+        id : 7,
+        name : 'Harsh'
+    },
+    {
+        id : 8,
+        name : 'Himanshu'
+    },
+    {
+        id : 9,
+        name : 'Girraj'
+    },
+    {
+        id : 10,
+        name : 'Ritik Agrawal'
+    },
+]
+const User = () : JSX.Element  => {
 
-const User = () : JSX.Element => {
-
-    const [name,setName] = useState("");
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const [display,setDisplay] = useState(false);
-    const [isShow,setIsShow] = useState(true);
     return (
         <View style={{
             padding : 10
         }}>
-            <Text style={{fontSize : 30,color : '#000'}}>Input Field</Text>
-            <Text style={{fontSize : 20,color : '#000',marginTop : 20}}>Enter Your Name : </Text>
-            <TextInput 
-                style={styles.inputStyle}
-                onChangeText={(value)=>{
-                    setName(value);
-                }}
-                value={name}
-            ></TextInput>
-            <Text style={{fontSize : 20,color : '#000',marginTop : 20}}>Enter Your Email : </Text>
-            <TextInput 
-                style={styles.inputStyle}
-                onChangeText={(value)=>{
-                    setEmail(value);
-                }}
-                value={email}
-                autoCapitalize='none'
-                keyboardType='email-address'
-               
-            ></TextInput>
-            
-            <Text style={{fontSize : 20,color : '#000',marginTop : 20}}>Enter Your Password : </Text>
-            <View style={{
-                marginBottom : 3
-            }}>
-
-            <TextInput 
-                style={styles.inputStyle}
-                onChangeText={(value)=>{
-                    setPassword(value);
-                }}
-                value={password}
-                secureTextEntry={isShow}
-                ></TextInput>
-                <Button 
-                    title='Show Password'
-                    onPress={()=>{
-                        setIsShow(!isShow);
-                    }}
-                ></Button>
-            </View>
-            
-
-            <Button title='Submit'
-            color={'red'}
-            onPress={()=>{
-                setDisplay(!display);
-            }}
-            touchSoundDisabled={true}
-            ></Button>
-            <Text ></Text>
-            <Button title='Clear' onPress={()=>{
-                setName("");
-                setEmail("");
-                setPassword("");
-            }}
-            touchSoundDisabled={true}
-            ></Button>
-            {
-                    display ? 
-            <View style={{
-                backgroundColor : 'green',
-                borderColor :'black',
-                borderWidth : 2,
-                borderBottomWidth : 10,
-                borderRadius : 5,
-                marginTop : 20
-            }}>
-                
-                    <View>
-                        <Text style={{fontSize : 20,color : '#fff',margin : 5}}>Name     : {name}</Text>
-                        <Text style={{fontSize : 20,color : '#fff',margin : 5}}>Email    : {email}</Text>
-                        <Text style={{fontSize : 20,color : '#fff',margin : 5}}>Password : {password}</Text>
-                    </View> 
-               
-            </View> : null
-             }
+            <Text style={{fontSize:30}}>List with FlatList</Text>
+            <FlatList
+            data={data}
+            renderItem={({item})=>(
+                <Text style={styles.listText}>{item.name}</Text>
+            )}
+            keyExtractor={item=>item.id}
+            ></FlatList>
         </View>
     )
 }
@@ -107,6 +74,15 @@ const styles = StyleSheet.create({
         marginBottom : 12,
         paddingLeft : 15,
         
+    },
+    listText : {
+        padding : 10,
+        fontSize : 20,
+        color : 'white',
+        backgroundColor : 'red',
+        marginBottom : 2,
+        borderTopLeftRadius :15,
+        borderBottomRightRadius : 15
     }
 })
 
