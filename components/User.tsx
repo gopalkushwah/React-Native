439cd5936e19663be69d141e239b8bf67b6ed913
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-06 18:28:46 +0530

    Create an dynamic array using loop

--- components/User.tsx
+++ components/User.tsx
@@ -6,63 +6,75 @@ import styles1 from './../style/style';
     
 */}
 const User = () : JSX.Element  => {
-    const userData = [
-        {
-            id : 1,
-            name : 'Name1',
-            data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
-        },
-        {
-            id : 2,
-            name : 'Name2',
-            data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
-        },
-        {
-            id : 3,
-            name : 'Name3',
-            data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
-        },
-        {
-            id : 4,
-            name : 'Name4',
-            data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
-        },
-        {
-            id : 5,
-            name : 'Name5',
-            data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
-        },
-        {
-            id : 6,
-            name : 'Name6',
-            data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
-        },
-        {
-            id : 7,
-            name : 'Name7',
-            data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
-        },
-        {
-            id : 8,
-            name : 'Name8',
-            data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
-        },
-        {
-            id : 9,
-            name : 'Name9',
-            data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
-        },
-        {
-            id : 10,
-            name : 'Name10',
-            data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
-        },
+    // const userData = [
+    //     {
+    //         id : 1,
+    //         name : 'Name1',
+    //         data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
+    //     },
+    //     {
+    //         id : 2,
+    //         name : 'Name2',
+    //         data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
+    //     },
+    //     {
+    //         id : 3,
+    //         name : 'Name3',
+    //         data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
+    //     },
+    //     {
+    //         id : 4,
+    //         name : 'Name4',
+    //         data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
+    //     },
+    //     {
+    //         id : 5,
+    //         name : 'Name5',
+    //         data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
+    //     },
+    //     {
+    //         id : 6,
+    //         name : 'Name6',
+    //         data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
+    //     },
+    //     {
+    //         id : 7,
+    //         name : 'Name7',
+    //         data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
+    //     },
+    //     {
+    //         id : 8,
+    //         name : 'Name8',
+    //         data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
+    //     },
+    //     {
+    //         id : 9,
+    //         name : 'Name9',
+    //         data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
+    //     },
+    //     {
+    //         id : 10,
+    //         name : 'Name10',
+    //         data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
+    //     },
 
 
 
-    ];
+    // ];
 
     
+    let d1 = [],d2 = [];
+    for(let i = 1 ; i< 20 ;i++){
+        const innerArray = [];
+        for (let j = 1; j <= 5; j++) {
+            innerArray.push(`Arr ${j}`);
+        }
+        d1.push({
+            id : i,
+            name : `Name ${i}`,
+            data : innerArray
+        })
+    }
     return (
         <View style={
             {
@@ -71,7 +83,7 @@ const User = () : JSX.Element  => {
              }}>
             <Text style={{color:'red',fontSize:30}}>SectionList</Text>
            <SectionList
-                sections={userData}
+                sections={d1}
                 renderItem={({item,index}) => (
                     <Text style={styles.li}>{index+1} . {item}</Text>
                 )}
