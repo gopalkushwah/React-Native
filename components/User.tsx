708f225f8b39cc75903530e4895904c64629bd2e
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-06 17:23:43 +0530

    Section List , nested array

--- components/User.tsx
+++ components/User.tsx
@@ -1,65 +1,98 @@
 import React from 'react'
-import { Text, View, StyleSheet, FlatList, TextInput, ScrollView } from 'react-native';
+import { Text, View, StyleSheet, FlatList, TextInput, ScrollView, SectionList } from 'react-native';
+import styles1 from './../style/style';
 
 {/*
     
 */}
 const User = () : JSX.Element  => {
+    const userData = [
+        {
+            id : 1,
+            name : 'Name1',
+            data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
+        },
+        {
+            id : 2,
+            name : 'Name2',
+            data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
+        },
+        {
+            id : 3,
+            name : 'Name3',
+            data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
+        },
+        {
+            id : 4,
+            name : 'Name4',
+            data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
+        },
+        {
+            id : 5,
+            name : 'Name5',
+            data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
+        },
+        {
+            id : 6,
+            name : 'Name6',
+            data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
+        },
+        {
+            id : 7,
+            name : 'Name7',
+            data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
+        },
+        {
+            id : 8,
+            name : 'Name8',
+            data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
+        },
+        {
+            id : 9,
+            name : 'Name9',
+            data : ['Arr1','Arr2','Arr3','Arr4','Arr5']
+        },
+        {
+            id : 10,
+            name : 'Name10',
+            data : ['Arr5','Arr4','Arr3','Arr2','Arr1']
+        },
 
-    const data = [];
 
-    for (let i = 1; i <= 50; i++) {
-      data.push({
-        id: i,
-        name: `Jaithalal ${i}`,
-        email: `jaithalal${i}@gmail.com`
-      });
-    }
+
+    ];
+
     
-
     return (
         <View style={
             {
                 margin: 5,
                 height : 810
              }}>
-            <Text style={{color:'black',fontSize:30}}>Componet in loop with FlatList</Text>
-            <FlatList 
-            data={data}
-            renderItem={({item})=>{
-                return(
-                    <UserData item={item}/>
-                )
-            }}
-            />
+            <Text style={{color:'red',fontSize:30}}>SectionList</Text>
+           <SectionList
+                sections={userData}
+                renderItem={({item,index}) => (
+                    <Text style={styles.li}>{index+1} . {item}</Text>
+                )}
+                renderSectionHeader={({section}) => (
+                    <Text style={styles.header}>{section.id} : {section.name}</Text>
+                )}
+           />
         </View>
     )
 }
 
-const UserData = (props: { item: any; }) =>{
-    const item = props.item;
-    return(
-        <View style={styles.box}>
-            <Text style={styles.text}>{item.name}</Text>
-            <Text style={styles.text}>{item.email}</Text>
-        </View>
-    )
-}
 
 const styles = StyleSheet.create({
-    text:{
+    li:{
         fontSize : 20,
         color :'black',
-        flex : 1,
-        margin : 2
+        marginLeft : 30
     },
-    box:{
-        flexDirection :'row',
-        borderWidth : 1,
-        borderColor:'black',
-        padding : 4,
-        marginBottom : 5,
-        borderRadius : 10
+    header:{
+        fontSize : 20,
+        color :'red',
     },
 })
 
