a980930aea7d9a1ba24cd02c0b3d5c9a73f8ff0d
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-06 14:21:10 +0530

    Component in loop with FlatList

--- components/User.tsx
+++ components/User.tsx
@@ -8,51 +8,59 @@ const User = () : JSX.Element  => {
 
     const data = [];
 
-    for (let i = 1; i <= 101; i++) {
+    for (let i = 1; i <= 50; i++) {
       data.push({
         id: i,
-        name: `Name ${i}`
+        name: `Jaithalal ${i}`,
+        email: `jaithalal${i}@gmail.com`
       });
     }
     
 
     return (
-        <View>
-            <Text style={{color:'black',fontSize:30}}> Grid with Dynamic data</Text>
-            <ScrollView style={{height : 780}}>
-            <View
-                style={styles.gridContainer}
-            >
-                {
-                    data.map((item) =>{
-                        return <Text style={styles.gridContainerText}>{item.name}</Text>
-                    })
-                }
-                
-            </View>
-            </ScrollView>
+        <View style={
+            {
+                margin: 5,
+                height : 810
+             }}>
+            <Text style={{color:'black',fontSize:30}}>Componet in loop with FlatList</Text>
+            <FlatList 
+            data={data}
+            renderItem={({item})=>{
+                return(
+                    <UserData item={item}/>
+                )
+            }}
+            />
+        </View>
+    )
+}
+
+const UserData = (props: { item: any; }) =>{
+    const item = props.item;
+    return(
+        <View style={styles.box}>
+            <Text style={styles.text}>{item.name}</Text>
+            <Text style={styles.text}>{item.email}</Text>
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    gridContainer : {
+    text:{
+        fontSize : 20,
+        color :'black',
         flex : 1,
-        flexDirection :'row',
-        flexWrap :'wrap',
-        marginLeft:11
+        margin : 2
     },
-    gridContainerText : {
-        color : 'white',
-        fontSize : 25,
-        backgroundColor :'blue',
-        margin : 5,
-        padding :5,
-        width : 120,
-        height : 120,
-        textAlign :'center',
-        textAlignVertical :'center'
-    }
+    box:{
+        flexDirection :'row',
+        borderWidth : 1,
+        borderColor:'black',
+        padding : 4,
+        marginBottom : 5,
+        borderRadius : 10
+    },
 })
 
 export default User;
