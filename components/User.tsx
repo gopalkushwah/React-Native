3ec16e18a7d015ffaf664d112bdcd1c6d0128d93
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-06 11:28:53 +0530

    Custom list using map method

--- components/User.tsx
+++ components/User.tsx
@@ -1,65 +1,103 @@
 import React from 'react'
-import { Text, View, StyleSheet, FlatList } from 'react-native';
+import { Text, View, StyleSheet, FlatList, TextInput, ScrollView } from 'react-native';
 
 {/*
     
 */}
-const data = [
-    {
-        id : 1,
-        name : 'Gopal'
-    },
-    {
-        id : 2,
-        name : 'Anish'
-    },
-    {
-        id : 3,
-        name : 'Satyam'
-    },
-    {
-        id : 4,
-        name : 'Ram'
-    },
-    {
-        id : 5,
-        name : 'Sumit'
-    },
-    {
-        id : 6,
-        name : 'Ritik'
-    },
-    {
-        id : 7,
-        name : 'Harsh'
-    },
-    {
-        id : 8,
-        name : 'Himanshu'
-    },
-    {
-        id : 9,
-        name : 'Girraj'
-    },
-    {
-        id : 10,
-        name : 'Ritik Agrawal'
-    },
-]
+// const data = [
+//     {
+//         id : 1,
+//         name : 'Gopal'
+//     },
+//     {
+//         id : 2,
+//         name : 'Anish'
+//     },
+//     {
+//         id : 3,
+//         name : 'Satyam'
+//     },
+//     {
+//         id : 4,
+//         name : 'Ram'
+//     },
+//     {
+//         id : 5,
+//         name : 'Sumit'
+//     },
+//     {
+//         id : 6,
+//         name : 'Ritik'
+//     },
+//     {
+//         id : 7,
+//         name : 'Harsh'
+//     },
+//     {
+//         id : 8,
+//         name : 'Himanshu'
+//     },
+//     {
+//         id : 9,
+//         name : 'Girraj'
+//     },
+//     {
+//         id : 10,
+//         name : 'Ritik Agrawal'
+//     },
+//     {
+//         id : 11,
+//         name : 'Ritik Agrawal'
+//     },
+//     {
+//         id : 12,
+//         name : 'Ritik Agrawal'
+//     },
+//     {
+//         id : 13,
+//         name : 'Ritik Agrawal'
+//     },
+// ]
 const User = () : JSX.Element  => {
+
+    const data = [];
+
+    for (let i = 1; i <= 1000; i++) {
+      data.push({
+        id: i,
+        name: `Name ${i}`
+      });
+    }
+    
 
     return (
         <View style={{
             padding : 10
         }}>
-            <Text style={{fontSize:30}}>List with FlatList</Text>
-            <FlatList
-            data={data}
-            renderItem={({item})=>(
-                <Text style={styles.listText}>{item.name}</Text>
-            )}
-            keyExtractor={item=>item.id}
-            ></FlatList>
+            
+            <Text style={{fontSize:30}}>Custom List using map method</Text>
+            
+            <ScrollView style={{marginBottom:30}}>
+            {
+                data.map((item)=>{
+                   return(
+                   <Text style={{fontSize:20,backgroundColor:'red',borderColor:'black',borderWidth:1,marginBottom:40,padding : 5,color:'white',borderRadius:5}}>{item.name}</Text>
+                   )
+                })
+            }
+            </ScrollView>
+            
+
+            {/* FlatList mentains scrollable view by defualt, we just need to set its height */}
+            {/* <FlatList style={{
+                height: 700
+            }}
+                data={data}
+                renderItem={({item})=>(
+                    <Text style={{fontSize:20,backgroundColor:'red',borderColor:'black',borderWidth:1,marginBottom:40,padding : 5,color:'white',borderRadius:5}}>{item.name}</Text>
+                )}
+            /> */}
+            
         </View>
     )
 }
@@ -73,7 +111,6 @@ const styles = StyleSheet.create({
         borderRadius : 5,
         marginBottom : 12,
         paddingLeft : 15,
-        
     },
     listText : {
         padding : 10,
