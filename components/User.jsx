2c17e0200ecb9b9afad3ff8045fd43b51fabfc8d
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-07 22:43:02 +0530

    useEffect hook as componentWillUnmount

--- components/User.jsx
+++ components/User.jsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import {View, Button, Text } from 'react-native';
-
+import {View, Button, Text,} from 'react-native';
 {/*
     we can use the useEffect hook to replicate the behavior of componentDidUpdate in class components. 
     componentDidUpdate is a lifecycle method that is called after a component has been updated (i.e., after a re-render caused by changes in state or props). 
@@ -9,22 +8,46 @@ import {View, Button, Text } from 'react-native';
 const User =() => {
 
     const [toggle,setToggle]=useState(false);
+    const [count,setCount]=useState(0);
     return (
-        <View style={{height:'100%',backgroundColor: toggle ? "black" : "white"}}>
-            <View 
-            style={{
-                margin : 10,
-                marginTop :'auto',
-                marginBottom :'auto'
-                }}
-            >
+        <View style={{height:'100%',backgroundColor: "white"}}>
+            <View>
+                <Text style={{fontSize:30,color : "black"}}>{count}</Text>
+
             <Button
+            touchSoundDisabled
             title='Toggle Color'
             onPress={()=>{
-                setToggle(!toggle)
+                setToggle(!toggle);
+                setCount(count+1);
             }}
             ></Button>
+            {
+                toggle ? <UserData toggle={toggle}/> : null
+            }
             </View>
+        </View>
+    )
+}
+
+const UserData = ({toggle}) =>{
+
+   let timer =  setInterval(()=>{
+        console.warn("interval");
+    },2000)
+    useEffect(()=>{
+        // console.warn("Hellow");
+
+
+        // to do the cleanup code we have to create a function
+        // this function will called automaticaly when component will be in the phase of unmounting
+        return () => {
+            clearInterval(timer);
+        }
+    })
+    return(
+        <View>
+            <Text style={{fontSize:30,color : "black"}}>Student</Text>
         </View>
     )
 }
