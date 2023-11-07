22f4520b4f2526fe06c3b25772e841e8481d1deb
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-07 16:04:08 +0530

    Toggle Color on click of button

--- components/User.jsx
+++ components/User.jsx
@@ -8,59 +8,25 @@ import {View, Button, Text } from 'react-native';
 */}
 const User =() => {
 
-    const [count,setCount]=useState(0);
-    const [data,setData]=useState(100);
-
-    const incrementCount = ()=>{
-        setCount(count + 1);
-    }
-    const incrementData = ()=>{
-        setData(data + 5);
-    }
+    const [toggle,setToggle]=useState(false);
     return (
-        <View style={{
-                margin: 5,
-                height : 810
-                }}>
-            <Text style={{fontSize:30,color:'red'}}>{count}</Text>
-            <Button 
-            title='Click Me Count'
-            touchSoundDisabled
-            onPress={incrementCount}
+        <View style={{height:'100%',backgroundColor: toggle ? "black" : "white"}}>
+            <View 
+            style={{
+                margin : 10,
+                marginTop :'auto',
+                marginBottom :'auto'
+                }}
+            >
+            <Button
+            title='Toggle Color'
+            onPress={()=>{
+                setToggle(!toggle)
+            }}
             ></Button>
-            
-            <Text style={{fontSize:30,color:'black'}}>{data}</Text>
-            <Button 
-            title='Click Me Data'
-            touchSoundDisabled
-            onPress={incrementData}
-            ></Button>
-
-            <UserContacts info={{count,data}}/>
+            </View>
         </View>
     )
 }
 
-
-
-const UserContacts = (props) =>{
-    const {count,data} = props.info;
-    useEffect(()=>{
-        console.warn("count has changed "+count);
-    },[count]);
-
-    useEffect(()=>{
-        console.warn("data has changed "+data);
-    },[data]);
-
-    return(
-        <View>
-            <Text style={{fontSize:40,color:'red'}}>Use Data Component</Text>
-            <Text style={{fontSize:30,color:'black'}}>{count}</Text>
-            <Text style={{fontSize:30,color:'green'}}>{data}</Text>
-        </View>
-
-    )
-    
-}
 export default User;
