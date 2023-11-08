ae1bd86189b79fd36996277d6e6cef1e76d33a04
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-08 15:08:26 +0530

    ActivityIndicator to show a loader

--- components/User.tsx
+++ components/User.tsx
@@ -1,56 +1,39 @@
 import React, { useState } from 'react'
-import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+import {ActivityIndicator, Button, StyleSheet, View} from 'react-native';
 
-const skills = [
-    {
-        id : 1,
-        skill : 'JAVA'
-    },
-    {
-        id : 2,
-        skill : 'PYTHON'
-    },
-    {
-        id : 3,
-        skill : 'NODE'
-    },
-    {
-        id : 4,
-        skill : 'HTML,CSS,JS'
-    },
-    {
-        id : 5,
-        skill : 'REACT.JS'
-    },
-    {
-        id : 6,
-        skill : 'REACT-NATIVE.JS'
-    },
-
-]
 const User = () => {
-    const [selectedRadio, setSelectedRadio] = useState(1);
+    const [showActivityIndicator, setShowActivityIndicator] = useState(false);
+    
+    const displayActivitiIndicator = () =>{
+        setShowActivityIndicator(true);
 
+
+        // just think we are calling an api here to fetch data from db and it will take time of 5 second
+        setTimeout(()=>{
+            setShowActivityIndicator(false);
+        },5000)
+
+    }
     return (
         <View style={styles.main}>
-          
-            {
-                skills.map((item,index)=>{
-                         return   <TouchableOpacity
-                        onPress={()=>setSelectedRadio(index)}
-                    >    
-                        <View style={styles.radioWrapper}>
-                            <View style={styles.radioBtn}>
-                                {
-                                    selectedRadio === index ? <View style={styles.radioBtnIn}></View> : null
-                                }
-                            </View>
-                            <Text style={styles.radioText}>{item.skill}</Text>
-                        </View>
-                        <Text></Text>
-                    </TouchableOpacity>
-                })
-            }
+          {/* <ActivityIndicator size={"small"} color={"red"} animating={true}></ActivityIndicator>
+          <ActivityIndicator size={"large"} color={"blue"} animating={true}></ActivityIndicator> */}
+
+
+          {/* <ActivityIndicator size={50} color={"red"} animating={true}></ActivityIndicator> */}
+
+{/* 
+          <ActivityIndicator size={100} color={"black"} animating={showActivityIndicator}></ActivityIndicator>
+          <Button 
+            title="click me"
+            onPress={displayActivitiIndicator}
+          ></Button> */}
+
+         {showActivityIndicator ? <ActivityIndicator size={100} color={"black"} ></ActivityIndicator> : null}
+          <Button 
+            title="click me"
+            onPress={displayActivitiIndicator}
+          ></Button>
         </View>
     )
 }
@@ -58,38 +41,8 @@ const User = () => {
 const styles = StyleSheet.create({
     main : {
         flex : 1,
-        // alignItems :'center',
+        alignItems :'center',
         justifyContent : 'center',
-        marginLeft : 20
-    },
-    radioBtn : {
-        height : 40,
-        width :60,
-        borderColor : 'red',
-        borderWidth : 1,
-        borderRadius : 20,
-        alignItems: 'center',
-        justifyContent :'center',
-        shadowColor :'red',
-        elevation : 20,
-        shadowOffset : {width : -20 ,height :-20}
-    },
-    radioBtnIn:{
-        height : 30,
-        width : 30,
-        backgroundColor : 'red',
-        borderRadius : 15,
-        
-    },
-    radioText : {
-        fontSize : 20,
-        color :'black',
-        marginLeft : 20
-    },
-    radioWrapper:{
-        alignItems:'center',
-        flexDirection:'row',
-        
     }
 })
 
