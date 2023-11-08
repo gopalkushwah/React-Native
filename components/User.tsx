2bc66c8574dbb2b969af8dbb0c501858d4671255
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-08 19:14:25 +0530

    Platform - checking the current platform , and some more properties

--- components/User.tsx
+++ components/User.tsx
@@ -1,54 +1,44 @@
 import React, { useState } from 'react'
-import {StyleSheet, View, Pressable, Alert, Text, StatusBar, Button} from 'react-native';
-{/*
-    the Pressable component is used to create interactive and touchable elements that respond to user presses and gestures. 
-    It allows you to wrap other components and provide touchable behavior to them, similar to buttons, images, or any other view elements. 
-    The Pressable component is highly customizable and provides options for handling various touch events.
-*/}
-const User = () => {
-    const [hide, setHideStatuBar] = useState(false);
-    const [colorStatus, setColorStatuBar] = useState('red');
-    const [animatedStatus, setAnimatedStatuBar] = useState(false);
-    const [barStyleStatus, setBarStyleStatuBar] = useState('default');
-    return (
-            <View style={styles.main}>
-                <StatusBar
-                 backgroundColor={colorStatus} 
-                 animated={animatedStatus}
-                 hidden={hide}
-                // barStyle={'dark-content'}
-                // barStyle={'light-content'}
-                barStyle={barStyleStatus}  //light-content
-                 ></StatusBar>
-                 <Button
-                 title='Change Status Bar Color'
-                 onPress={()=>{
-                    setColorStatuBar(colorStatus === 'red' ? 'blue' : 'red');
-                 }}
-                 ></Button>
-                 <Text></Text>
-                 <Button
-                 title='Hide Status Bar'
-                 onPress={()=>{
-                    setHideStatuBar(!hide);
-                 }}
-                 ></Button>
-                 <Text></Text>
-                 <Button
-                 title='Change Bar Style'
-                 onPress={()=>{
-                    setBarStyleStatuBar(barStyleStatus === 'default' ? 'dark-content' : 'default');
-                 }}
-                 ></Button>
-                 <Text></Text>
-                 <Button
-                 title='Set Animated'
-                 onPress={()=>{
-                    setAnimatedStatuBar(!animatedStatus);
-                 }}
-                 ></Button>
-            </View>
+import {StyleSheet, View,Platform, Button, Text} from 'react-native';
 
+const User = () => {
+
+    const [platform , setPlatform] = useState('android');
+    return (
+        <View style={[styles.main, {backgroundColor :  Platform.OS === platform ? 'red' :'blue'}]}>
+            <View 
+                style={{
+                    backgroundColor :'red'
+                }}
+            >
+                <Button
+                title="Click Me"
+                color={'red'}
+                onPress={()=>{
+                    setPlatform( platform === 'android' ? 'ios' : 'android');
+                }}
+                ></Button>
+                
+            </View>
+            <Text
+                    style={{
+                    color :'white',
+                    fontSize : 30
+                }}
+                >{platform}</Text>
+            <Text
+                    style={{
+                    color :'white',
+                    fontSize : 30
+                }}
+                >Platform Version : {Platform.Version}</Text>
+            {/* <Text
+                    style={{
+                    color :'white',
+                    fontSize : 30
+                }}
+                >Platform : {JSON.stringify(Platform)}</Text> */}
+        </View>
     )
 }
 
