39aeb53d7b313b68e5699a5906c447774ac19abc
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-08 18:45:43 +0530

    Status Bar

--- components/User.tsx
+++ components/User.tsx
@@ -1,52 +1,54 @@
 import React, { useState } from 'react'
-import {StyleSheet, View, Pressable, Alert, Text} from 'react-native';
+import {StyleSheet, View, Pressable, Alert, Text, StatusBar, Button} from 'react-native';
 {/*
     the Pressable component is used to create interactive and touchable elements that respond to user presses and gestures. 
     It allows you to wrap other components and provide touchable behavior to them, similar to buttons, images, or any other view elements. 
     The Pressable component is highly customizable and provides options for handling various touch events.
 */}
 const User = () => {
-    const [onHoverInEff, setOnHoverIn] = useState('red')
+    const [hide, setHideStatuBar] = useState(false);
+    const [colorStatus, setColorStatuBar] = useState('red');
+    const [animatedStatus, setAnimatedStatuBar] = useState(false);
+    const [barStyleStatus, setBarStyleStatuBar] = useState('default');
     return (
-        <View style={styles.main}>
-            <View>
-                <Pressable
-                    onPress={()=>{
-                        Alert.alert("On Press called")
-                        
-
-                    }}
-                    onLongPress={()=>{
-                        Alert.alert("On Long Press called");
-                        setOnHoverIn('black');
-                    }}
-                    onHoverIn={()=>{
-                        setOnHoverIn('black');
-                    }}
-                    delayLongPress={500}
-                    // onPressIn={()=>{
-                    //     Alert.alert("On Press In called")
-                    // }}
-                    // onPressOut={()=>{
-                    //     Alert.alert("On Press Out called")
-                    // }}
-                    android_ripple={{ color: 'pink' }}
-                    style={{
-                        backgroundColor:onHoverInEff,
-                        borderRadius : 5,
-                        
-                    }}
-                >
-                    <Text style={{
-                        fontSize : 30,
-                        fontWeight :'bold',
-                        color:'white',
-                        padding : 5,
-                        
-                    }}>Pressable Button</Text>
-                </Pressable>
+            <View style={styles.main}>
+                <StatusBar
+                 backgroundColor={colorStatus} 
+                 animated={animatedStatus}
+                 hidden={hide}
+                // barStyle={'dark-content'}
+                // barStyle={'light-content'}
+                barStyle={barStyleStatus}  //light-content
+                 ></StatusBar>
+                 <Button
+                 title='Change Status Bar Color'
+                 onPress={()=>{
+                    setColorStatuBar(colorStatus === 'red' ? 'blue' : 'red');
+                 }}
+                 ></Button>
+                 <Text></Text>
+                 <Button
+                 title='Hide Status Bar'
+                 onPress={()=>{
+                    setHideStatuBar(!hide);
+                 }}
+                 ></Button>
+                 <Text></Text>
+                 <Button
+                 title='Change Bar Style'
+                 onPress={()=>{
+                    setBarStyleStatuBar(barStyleStatus === 'default' ? 'dark-content' : 'default');
+                 }}
+                 ></Button>
+                 <Text></Text>
+                 <Button
+                 title='Set Animated'
+                 onPress={()=>{
+                    setAnimatedStatuBar(!animatedStatus);
+                 }}
+                 ></Button>
             </View>
-        </View>
+
     )
 }
 
