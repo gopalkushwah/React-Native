eae5d59585cc7809e98dc148bd852f4071c37295
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-09 08:21:59 +0530

    WebView - Open any website in our application

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { 
+  StatusBar,
+  Text,
   View
  } from "react-native";
-import User from "./components/User";
+// import User from "./components/User";
+import { WebView } from 'react-native-webview';
 const App = () => {
   return (
-    <View style={{flex : 1}}>
-      <User></User>
+    <View style={{flex:1}}>
+      <StatusBar
+      backgroundColor={'blue'}
+
+      ></StatusBar>
+      <WebView source={{uri : 'https://panditsoft.in/'}}></WebView>
     </View>
   )
 }
--- components/User.tsx
+++ components/User.tsx
@@ -1,43 +1,12 @@
 import React, { useState } from 'react'
-import {StyleSheet, View,Platform, Button, Text} from 'react-native';
+import {StyleSheet, View} from 'react-native';
+import WebView from 'react-native-webview';
 
 const User = () => {
 
-    const [platform , setPlatform] = useState('android');
     return (
-        <View style={[styles.main, {backgroundColor :  Platform.OS === platform ? 'red' :'blue'}]}>
-            <View 
-                style={{
-                    backgroundColor :'red'
-                }}
-            >
-                <Button
-                title="Click Me"
-                color={'red'}
-                onPress={()=>{
-                    setPlatform( platform === 'android' ? 'ios' : 'android');
-                }}
-                ></Button>
-                
-            </View>
-            <Text
-                    style={{
-                    color :'white',
-                    fontSize : 30
-                }}
-                >{platform}</Text>
-            <Text
-                    style={{
-                    color :'white',
-                    fontSize : 30
-                }}
-                >Platform Version : {Platform.Version}</Text>
-            {/* <Text
-                    style={{
-                    color :'white',
-                    fontSize : 30
-                }}
-                >Platform : {JSON.stringify(Platform)}</Text> */}
+        <View>
+            
         </View>
     )
 }
