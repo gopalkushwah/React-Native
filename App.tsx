0fe16d52db18638a7314c1013f93db9e63660753
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-10 00:49:27 +0530

    fetch api - Fetch data from database using api

--- App.tsx
+++ App.tsx
@@ -1,53 +1,12 @@
-import { View,Image,Pressable } from "react-native";
+import { View} from "react-native";
 import React from "react";
 
 import User from "./components/User";
-import { NavigationContainer } from '@react-navigation/native';
-import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
-import Message from "./components/Message";
-import Groups from "./components/Goups";
-import Calls from "./components/Calls";
-import Community from "./components/Community";
-import Status from "./components/Status";
-// import MessageImage from "./assets/message.png";
-// import GroupsImage from "./assets/community.png";
-// import CallImage from "./assets/call.png";
-// import StatusImage from "./assets/status.png";
-// import UserImage from "./assets/user.png";
-// import Menu from "./assets/blackmenu.png";
-
-const Tab = createMaterialTopTabNavigator();
 const App = () => {
   return (
-    <NavigationContainer>
-      <Tab.Navigator
-      
-      screenOptions={{
-        // tabBarShowLabel :false,
-       
-        tabBarLabelStyle:{
-          fontSize:15,
-          color :'black',
-          fontWeight : '800'
-        },
-        
-        
-      }}
-      >
-        <Tab.Screen 
-        name="Message"  
-        component={Message}
-        />
-        <Tab.Screen
-        name="Groups" component={Groups}/>
-        <Tab.Screen
-        name="Calls" component={Calls}/>
-        <Tab.Screen
-        name="Community" component={Community}/>
-        <Tab.Screen
-        name="Status" component={Status}/>
-      </Tab.Navigator>
-    </NavigationContainer>
+    <View>
+      <User/>
+    </View>
   )
 }
 
--- components/User.tsx
+++ components/User.tsx
@@ -1,12 +1,42 @@
-import React from 'react'
-import { View,Text,Button,StyleSheet,TextInput } from 'react-native'
+import React, { useEffect } from 'react'
+import { View,Text,Button,StyleSheet,TextInput, ScrollView, ActivityIndicator } from 'react-native'
 import { useState } from 'react';
 
 const User = () => {
+const [posts,setPosts] = useState([]);
+const [indicator,setIndicator] = useState(false);
+    const getPost= async ()=>{
+        const url = "https://jsonplaceholder.typicode.com/posts";
+        setIndicator(true);
+        let result = await fetch(url);
+        setIndicator(false);
+        result = await result.json();
+        // console.warn(result);
+        
+        setPosts(result);
+    }
+    useEffect(()=>{
+        getPost();
+    },[])
     return (
+        <ScrollView>
+        {
+           indicator ? <ActivityIndicator size={100} accessible={false}></ActivityIndicator> : null
+        }
         <View >
-            <Text>Hello</Text>
+            {
+                posts.map((post,index)=>{
+                    return <View key={index}>
+                        <Text style={{fontSize:30,color :'black'}}>ID : {post.id}</Text>
+                        <Text style={{fontSize:30,color :'black'}}>Name : {post.title}</Text>
+                        <Text style={{fontSize:30,color :'black'}}></Text>
+                        <Text style={{fontSize:30,color :'black'}}>Description : {post.body}</Text>
+
+                    </View>
+                })
+            }
         </View>
+        </ScrollView>
     )
 }
 const styles = StyleSheet.create({
