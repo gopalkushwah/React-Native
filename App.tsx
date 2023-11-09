0646cee5d71340be42a8a84af2a4e8b3e86b895f
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-09 19:30:16 +0530

    Adding custom icons in and Component in Stack Navigation

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
@@ -6,18 +6,51 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Login from "./components/Login";
 import User from "./components/User";
 import Home from "./components/Home";
-
+import { Button, Image, Text, View } from "react-native";
+import WhiteMenu from './assets/whitemenu.png';
+import BlackMenu from './assets/blackmenu.png';
+import Sun from './assets/sun.png';
+import Moon from './assets/moon.png';
 
 const Stack = createNativeStackNavigator();
 const App = () => {
+
+  const [mod ,setMod] = useState(false);
+  const [modColor ,setModColor] = useState({bg : 'black',cl : 'white'});
   return (
     <NavigationContainer>
       <Stack.Navigator
+        
         screenOptions={{
+          headerTitle:()=><Text style={{height : 40}} onPress={()=>console.warn("clicked")}><Image 
+              source={mod ? BlackMenu : WhiteMenu}
+              style={{width : 30,height : 30}}
+            ></Image>
+          </Text>,
+          headerRight : ()=><View style={{flexDirection : 'row'}} >
+          <Text style={{height : 40}} onPress={()=>{
+              setMod(!mod);
+              setModColor(modColor.bg === 'black' ? {bg:'white',cl:'black'} : {bg:'black',cl:'white'})
+            }}><Image 
+            source={mod ? Sun : Moon}
+            style={{width : 30,height : 30}}
+              />
+          </Text>
+          <Text style={{height : 40,marginLeft : 20}} onPress={()=>{
+              setMod(!mod);
+              setModColor(modColor.bg === 'black' ? {bg:'white',cl:'black'} : {bg:'black',cl:'white'})
+            }}><Image 
+            source={mod ? Sun : Moon}
+            style={{width : 30,height : 30}}
+              />
+          </Text>
+          </View>
+          ,
+          
           headerStyle:{
-            backgroundColor : 'gold'
+            backgroundColor : modColor.bg
           },
-          headerTintColor:'white',
+          headerTintColor:modColor.cl,
           headerTitleStyle:{
             fontSize :30,
             fontWeight : 'bold'
@@ -28,10 +61,7 @@ const App = () => {
           name="Login" 
           component={Login}
           options={{
-            headerStyle:{
-              backgroundColor : 'blue'
-            },
-            headerTintColor:'white',
+            
             headerTitleStyle:{
               fontSize :30,
               fontWeight : 'bold'
@@ -41,17 +71,17 @@ const App = () => {
         />
         <Stack.Screen name="User" component={User}
         options={{
-          headerStyle:{
-            backgroundColor : 'green'
-          },
-          headerTintColor:'white',
+          
           headerTitleStyle:{
-            fontSize :20,
+            fontSize :30,
             fontWeight : 'bold'
           }
         }}
         />
-        <Stack.Screen name="Home" component={Home}/>
+        <Stack.Screen name="Home"
+        
+        
+        component={Home}/>
       </Stack.Navigator>
     </NavigationContainer>
   )
