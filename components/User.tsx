c170d69439630abf440338d342e1c193b5af6096
gopalkushwah <gopalkushwah15101999@gmail.com>
2023-11-10 13:36:17 +0530

    Take input from TextInput and save to database

--- components/User.tsx
+++ components/User.tsx
@@ -4,13 +4,16 @@ import { useState } from 'react';
 
 const User = () => {
     const [posts,setPosts] = useState({id:"",title:"",body:""});
+    const [input,setInput] = useState({title:"",body:""});
     const [indicator,setIndicator] = useState(false);
     const savePost= async ()=>{
-        setIndicator(true);
+        // setIndicator(true);
         const post = {
-            "title": "Hello I am Sumit  learning javasript",
-            "body": "Javasript is programming language"
+            "title": input.title,
+            "body": input.body
         };
+        // console.warn(post);
+
         const url = "http://10.0.2.2:8080/save-posts";
         let result = await fetch(url,{
             method :'POST',
@@ -33,8 +36,35 @@ const User = () => {
             </View> : null
             
         }
+        <View style={{margin :20}}>
+            <TextInput
+            style={{
+                marginBottom : 10,
+                padding : 15,
+                borderRadius : 10,
+                borderColor:'black',
+                borderWidth : 1,
+                fontSize : 20
+            }}
+            onChangeText={(text)=>setInput((prevData)=>({ ...prevData, title: text }))}
+            placeholder='Enter title'></TextInput>
+            <TextInput
+            style={{
+                marginBottom : 10,
+                padding : 15,
+                borderRadius : 10,
+                borderColor:'black',
+                borderWidth : 1,
+                fontSize : 20
+            }}
+            onChangeText={(text)=>setInput((prevData)=>({ ...prevData, body: text }))}
+            placeholder='Enter body'></TextInput>
+            <View style={{borderRadius : 10}}>
+                <Button title='Save Data'  onPress={savePost}></Button>
+            </View>
+        </View>
         <View>
-            <Button title='Save Data' onPress={savePost}></Button>
+
             <Text style={{fontSize:30,color:'black'}}>ID : {posts.id}</Text>
             <Text style={{fontSize:30,color:'black'}}>TITLE : {posts.title}</Text>
             <Text style={{fontSize:30,color:'black'}}>BODY : {posts.body}</Text>
